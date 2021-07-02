89f453eb0f9adb6b366a850dda62f8ae0eb429ef
Jenny Wauer <jkfoil1@gmail.com>
2021-07-02 14:28:55 -0500

    add user  model

--- help_desk_system/models.py
+++ help_desk_system/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+import re
 
-# Create your models here.
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
+        
+class User(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField()
+    password = models.CharField(max_length=255)
+    admin_level = models.IntegerField(validators=[validate_max, validate_min], default=1)
+    role = models.CharField(max_length=255, default="")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    objects = UserManager()
