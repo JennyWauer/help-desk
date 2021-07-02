dad6d4221ea5228fcde94e9d3325c68d29d1cf7b
Jenny Wauer <jkfoil1@gmail.com>
2021-07-02 16:15:29 -0500

    add register widgets

--- help_desk_system/forms.py
+++ help_desk_system/forms.py
@@ -5,7 +5,7 @@ from django import forms
 from .models import User
 
 class RegisterForm(ModelForm):
-    confirm_password = forms.CharField(max_length=255)
+    confirm_password = forms.CharField(max_length=255, widget=forms.PasswordInput(attrs={'class': 'form-control'}))
     class Meta:
         model = User
         fields = (
@@ -15,6 +15,13 @@ class RegisterForm(ModelForm):
             "password",
             "confirm_password",
         )
+
+        widgets = {
+            "first_name": forms.TextInput(attrs={'class': 'form-control'}),
+            "last_name": forms.TextInput(attrs={'class': 'form-control'}),
+            "email": forms.TextInput(attrs={'class': 'form-control'}),
+            "password": forms.PasswordInput(attrs={'class': 'form-control'}),
+        }
 
 class LoginForm(ModelForm):
     class Meta:
